b7d8692663c7221d948446a169502deeae514691
mlzx <mlzx1991@163.com>
2014-06-27 17:11:05 +0800

    other

---
+++ src/com/legoo/haier/AsyncTask/QuestionAsyncTask.java
@@ -0,0 +1,71 @@
+package com.legoo.haier.AsyncTask;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import com.legoo.haier.Application.Haier;
+import com.legoo.haier.AsyncTask.Base.JsonEvent;
+import com.legoo.haier.AsyncTask.Base.NetworkAsyncTask;
+import com.legoo.haier.Handler.ValueJsonHandler;
+import com.legoo.haier.Handler.Json.JsonHandler;
+import com.legoo.haier.Handler.Json.JsonOperation;
+
+
+/**
+ * @class Register AsyncTask
+ * @author LeonNoW
+ * @version 1.0
+ * @date 2014-02-15
+ */
+public class QuestionAsyncTask extends NetworkAsyncTask 
+{
+	private static final String NAME = "name";
+	private static final String PASSWORD = "PASSWORD";
+	
+	private String _name;
+	private String _password;
+
+	
+	public QuestionAsyncTask(String name, String password)
+	{
+		super();
+		_name = name;
+		_password = password;
+	}
+	
+	@Override
+	protected JsonEvent doInBackground(Void... params) 
+	{   
+		JsonEvent event = new JsonEvent(this);
+		event.setMark(super.getMark());
+//		String url = Haier.getInstance().getDataService().getRegister();
+		String url = "";
+		if (url != null)
+		{
+			List<NameValuePair> pairs = new ArrayList<NameValuePair>(); 
+		    pairs.add(new BasicNameValuePair(NAME, _name));
+		    pairs.add(new BasicNameValuePair(PASSWORD, _password));
+		    
+			ValueJsonHandler handler;
+			do
+			{
+				handler = (ValueJsonHandler) JsonOperation.post(url, pairs, new ValueJsonHandler());
+			}
+			while (retryTask(handler) == true);
+			
+			event.setError(handler.getError());
+			event.setMessage(handler.getMessage());
+
+			pairs.clear();
+			pairs = null;
+			url = null;
+			handler = null;
+		}
+		else 
+		{
+			event.setError(JsonHandler.ERROR_CONNECTION);
+		}
+		return event;
+	}
+}
